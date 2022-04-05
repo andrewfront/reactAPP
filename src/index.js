237787228c595978f808250112846e6c26ad6908
unknown <andrew.ndrw1@gmail.com>
2022-04-05 22:28:53 +0300

    exmple component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
+// import React from 'react';
 import ReactDom from 'react-dom';
 function Greeting() {
-  return <h4>Hello World</h4>
+  return (
+    <div>
+      <Person></Person>
+      <Message></Message>
+    </div>
+  )
 }
+// const Greeting =() => {
+//   return React.createElement('div', {}, React.createElement('h1', {}, 'Hello'))
+// }
+const Person = () => <h2>Peter Watson</h2>
+const Message = () => {return <p>this is my message</p>}
 ReactDom.render(<Greeting></Greeting>, document.getElementById('root'))
